bf80e45f74ecea8bde95cb69d2fee6b6287993a9
Olha Kramarenko <okramarenko-ua@singlestore.com>
2024-02-27 14:23:24 +0200

    Fix for the sample to work with SingleStore 8.0
    Test Plan: n/a
    
    Reviewers: pmishchenko-ua
    
    Reviewed By: pmishchenko-ua
    
    Subscribers: engineering-list
    
    Differential Revision: https://grizzly.internal.memcompute.com/D66975

--- Models/Message.cs
+++ Models/Message.cs
@@ -8,6 +8,11 @@ namespace SingleStoreORM.Models
     {
         public long Id { get; set; }
         public string Content { get; set; }
-        public DateTime CreateDate { get; set; }
+
+        // We manually set the default value for this field because SingleStore's zero-value for DateTime type exceeds
+        // the default value for DateTime in .NET. Beginning with SingleStore 8.0, the global sync variable data_conversion_compatibility_level
+        // controls the way certain data conversions are performed. Consequently, SingleStore refrains from converting out-of-range timestamps
+        // unless you explicitly modify the data_conversion_compatibility_level value (https://docs.singlestore.com/cloud/reference/sql-reference/data-types/data-type-conversion/)
+        public DateTime CreateDate { get; set; } = new DateTime(1001, 1, 1, 0, 0, 0, 0);
     }
 }
